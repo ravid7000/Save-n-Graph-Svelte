f0c88f1e40ebfce7cf7732bc509124fb900db906
Ravi Dhiman <ravid7000@gmail.com>
2023-10-17 00:50:07 +0530

    tailwind setup

---
+++ src/lib/ui/Layout/Main.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import '$lib/ui/Styles/tailwind-init.css'
+
+  import Navbar from './Navbar.svelte'
+  import Footer from './Footer.svelte'
+</script>
+
+<Navbar />
+<main>
+  <slot />
+</main>
+<Footer />
